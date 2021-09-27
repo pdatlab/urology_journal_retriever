c4b0fcd758e893fb0658d82b20a6a93989ee5840
JacobNBraun <89799192+JacobNBraun@users.noreply.github.com>
2021-09-27 09:45:14 -0500

    Update JournalGrabber.py
    Addition of file mover/downloader

--- JournalGrabber.py
+++ JournalGrabber.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import time
 import os
 import shutil
 
@@ -20,6 +21,9 @@ profile.update_preferences()
 
 driver = webdriver.Firefox(firefox_profile=profile, executable_path=r"C:\Program Files\Mozilla Firefox\Driver\geckodriver.exe")
 # driver loaded with specified profile and webdriver path
+
+start_year = int()
+volume_issue = []
 
 def _years(start_year,end_year):
     while start_year <= end_year:
@@ -39,6 +43,8 @@ def _issues():
         issue_list = driver.find_elements_by_class_name('visitable')
         issue_list[issue_num].click()
         volume_issue = driver.find_element_by_class_name('cover-image__parent-item').text
+        volume_issue.split(',')
+        volume_issue.replace(' ','_')
         WebDriverWait(driver, 10).until(EC.presence_of_element_located
                                         ((By.CLASS_NAME, "issue-item__title"))) 
         article_list = driver.find_elements_by_class_name('issue-item__title')
@@ -49,7 +55,7 @@ def _issues():
         while article_num <= len(article_list):
             article_list = driver.find_elements_by_class_name('issue-item__title')
             article_list[article_num].click()
-            # _infograbber()
+            # _infoGrabber()
             driver.get(article_link)
             WebDriverWait(driver, 10).until(EC.presence_of_element_located
                                         ((By.CLASS_NAME, "issue-item__title")))
@@ -57,22 +63,40 @@ def _issues():
         driver.get(issue_link)
 
         
-def _infograbber():
+def _infoGrabber():
     title = driver.find_element_by_class_name('citation__title').text
     page = driver.find_element_by_class_name('page-range').text
     driver.find_element_by_class_name('coolBar__ctrl pdf-download').click()
     driver.send_keys("g")
     # shortcut for opening a download popup in the BJUI journal viewer
-    # TODO Rename file to title+page?
-    # shutil.move(download path,requested path)
+    
+    filename = _fileName(10)
+    source = "c:\\BJUI\landing" f"\{filename}" ".pdf" 
+    destination =  "c:\\BJUI\landing" f"\{volume_issue[0]}" f"_{start_year}" f"\{volume_issue[1]}"
+    f"\{title}" ".pdf" 
+    path = os.path.join("c:\\", destination)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    # looks for existing file path, creates if absent    
+    shutil.move(source,destination)
+  
+def _fileName(waitTime):
+    # driver.execute_script("window.open()")
+    # WebDriverWait(driver,10).until(EC.new_window_is_opened)
+    # driver.switch_to.window(driver.window_handles[-1])
+    driver.get("about:downloads")
+    endTime = time.time()+waitTime
+    while True:
+        try:
+            fileName = driver.execute_script("return document.querySelector('#contentAreaDownloadsView .downloadMainArea .downloadContainer description:nth-of-type(1)').value")
+            if fileName:
+                return fileName
+        except:
+            pass
+        time.sleep(1)
+        if time.time() > endTime:
+            break
+        # this goes into downloads and finds the name of the most recently downloaded file
 _years(1996, 1997)
 
-"""
-EXAMPLE of firefox autodownload solution from stackx 
-(link:https://sqa.stackexchange.com/questions/2197/how-to-download-a-file-using-seleniums-webdriver)
-
-I do not think download directory can be changed while in a loop. Files may have to be sorted by assigned name
-    Assigning name to file by renaming last downloaded (Link:
-                                            https://stackoverflow.com/questions/34548041/selenium-give-file-name-when-downloading)
-"""
 
